aeb5d8b50eb610486ddb2cce6d03c392351ba20a
GaryFrazier95 <garyfrazier95@yahoo.com>
2022-03-30 18:01:09 +0100

    add error logging

--- src/main.c
+++ src/main.c
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 #include "glfw3.h"
 
+void glfwErrorCallback(int error, const char* description) {
+	fprintf(stderr, "Error code %d: %s\n", error, description);
+}
+
 int main() {
+	// Initialization
 	if (!glfwInit()) {
 		printf("glfw initialization error\n");
 		getchar();
 		exit(0);
 	}	
-
+	glfwSetErrorCallback(glfwErrorCallback);
 	printf("glfw initialized\n");
 	getchar();
+	glfwTerminate();
 }
 
